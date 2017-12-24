d83f170af2f0dac3da9404cb1c5ef6a5664d492a
twinbird <ixa2063@gmail.com>
2017-12-24 23:44:24 +0900

    ファイル名を変更。フォームを追加

---
+++ SalesManagement/Form/CompanyInformationEntry.vb
@@ -0,0 +1,3 @@
+﻿Public Class CompanyInformationEntry
+
+End Class
---
+++ SalesManagement/Form/SalesRateEntry.Designer.vb
@@ -0,0 +1,29 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SalesRateEntry
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Text = "SalesRateEntry"
+    End Sub
+End Class

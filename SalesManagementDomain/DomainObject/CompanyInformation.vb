6e64c1d7f76d931d5bbd7eea06f70564102d919e
twinbird <ixa2063@gmail.com>
2017-12-27 17:53:39 +0900

    もろもろ修正

---
+++ SalesManagementDomain/DomainObject/CompanyInformation.vb
@@ -0,0 +1,74 @@
+﻿Option Strict On
+Option Infer On
+
+Imports System.Collections.Generic
+Imports System.ComponentModel
+
+''' <summary>
+''' 特定日付時点の自社情報を表すクラス
+''' </summary>
+Public Class CompanyInformation
+    Implements IDataErrorInfo
+
+#Region "定数"
+
+#End Region
+
+#Region "コンストラクタ"
+
+    Public Sub New()
+    End Sub
+
+#End Region
+
+#Region "値プロパティ"
+
+#End Region
+
+#Region "エラープロパティ"
+
+    ''' <summary>
+    ''' エラー保持変数
+    ''' </summary>
+    Private _errors As New Dictionary(Of String, String)
+
+
+    ''' <summary>
+    ''' このオブジェクトの状態にエラーがあればTrue
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function HasError() As Boolean
+        Return _errors.Count <> 0
+    End Function
+
+    ''' <summary>
+    ''' 指定プロパティにエラーがあればメッセージを返す
+    ''' </summary>
+    ''' <param name="columnName"></param>
+    ''' <returns></returns>
+    Default Public ReadOnly Property Item(columnName As String) As String Implements IDataErrorInfo.Item
+        Get
+            If _errors.ContainsKey(columnName) = False Then
+                Return String.Empty
+            End If
+            Return _errors(columnName)
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' このオブジェクト全体で整合性を保てていないエラーがあればメッセージを返す
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property [Error] As String Implements IDataErrorInfo.Error
+        Get
+            Return String.Empty
+        End Get
+    End Property
+
+#End Region
+
+#Region "検証メソッド"
+
+#End Region
+
+End Class

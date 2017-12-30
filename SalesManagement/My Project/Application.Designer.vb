ec970c80f19312880996ed0ebe91a27d083d6840
twinbird <ixa2063@gmail.com>
2017-12-30 11:09:47 +0900

    名前を変更

--- SalesManagement/My Project/Application.Designer.vb
+++ SalesManagement/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SalesManagement.MainMDI
+            Me.MainForm = Global.Application.MainMDI
         End Sub
     End Class
 End Namespace

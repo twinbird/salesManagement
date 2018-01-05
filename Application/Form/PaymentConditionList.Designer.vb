97fb13ec01b8b4b9164b6f7126f613ea4c1551c7
twinbird <ixa2063@gmail.com>
2018-01-06 04:10:13 +0900

    支払条件の管理画面をUI変更

--- Application/Form/PaymentConditionList.Designer.vb
+++ Application/Form/PaymentConditionList.Designer.vb
@@ -22,54 +22,39 @@ Partial Class PaymentConditionList
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.EditButton = New System.Windows.Forms.DataGridViewButtonColumn()
         Me.PaymentName = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.CutOff = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.PaymentMonth = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.PaymentDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SearchGroupBox = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ClearSearcConditionButton = New System.Windows.Forms.Button()
+        Me.SearchButton = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.GroupBox1.SuspendLayout()
+        Me.SearchGroupBox.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.EditButton, Me.PaymentName, Me.CutOff, Me.PaymentMonth, Me.PaymentDate})
-        Me.DataGridView1.Location = New System.Drawing.Point(10, 63)
+        Me.DataGridView1.Location = New System.Drawing.Point(10, 92)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 21
-        Me.DataGridView1.Size = New System.Drawing.Size(477, 186)
+        Me.DataGridView1.Size = New System.Drawing.Size(565, 325)
         Me.DataGridView1.TabIndex = 5
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Location = New System.Drawing.Point(29, 12)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(305, 45)
-        Me.GroupBox1.TabIndex = 4
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "フィルタ"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(16, 17)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(278, 19)
-        Me.TextBox1.TabIndex = 2
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(351, 27)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(136, 23)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "新しい支払条件を登録"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'EditButton
         '
@@ -96,25 +81,101 @@ Partial Class PaymentConditionList
         Me.PaymentDate.HeaderText = "支払日"
         Me.PaymentDate.Name = "PaymentDate"
         '
+        'SearchGroupBox
+        '
+        Me.SearchGroupBox.Controls.Add(Me.SearchButton)
+        Me.SearchGroupBox.Controls.Add(Me.ClearSearcConditionButton)
+        Me.SearchGroupBox.Controls.Add(Me.Label1)
+        Me.SearchGroupBox.Controls.Add(Me.TextBox1)
+        Me.SearchGroupBox.Location = New System.Drawing.Point(29, 12)
+        Me.SearchGroupBox.Name = "SearchGroupBox"
+        Me.SearchGroupBox.Size = New System.Drawing.Size(404, 74)
+        Me.SearchGroupBox.TabIndex = 4
+        Me.SearchGroupBox.TabStop = False
+        Me.SearchGroupBox.Text = "検索"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(90, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(227, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(439, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(136, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "新しい支払条件を登録"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 419)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(587, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel
+        '
+        Me.ToolStripStatusLabel.Name = "ToolStripStatusLabel"
+        Me.ToolStripStatusLabel.Size = New System.Drawing.Size(113, 17)
+        Me.ToolStripStatusLabel.Text = "ToolStripStatusLabel"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 12)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "支払条件名"
+        '
+        'ClearSearcConditionButton
+        '
+        Me.ClearSearcConditionButton.Location = New System.Drawing.Point(323, 16)
+        Me.ClearSearcConditionButton.Name = "ClearSearcConditionButton"
+        Me.ClearSearcConditionButton.Size = New System.Drawing.Size(75, 23)
+        Me.ClearSearcConditionButton.TabIndex = 8
+        Me.ClearSearcConditionButton.Text = "クリア"
+        Me.ClearSearcConditionButton.UseVisualStyleBackColor = True
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(323, 42)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 8
+        Me.SearchButton.Text = "検索"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
         'PaymentConditionList
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(503, 261)
+        Me.ClientSize = New System.Drawing.Size(587, 441)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SearchGroupBox)
         Me.Controls.Add(Me.Button1)
         Me.Name = "PaymentConditionList"
         Me.Text = "支払条件の管理"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        Me.SearchGroupBox.ResumeLayout(False)
+        Me.SearchGroupBox.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents SearchGroupBox As GroupBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents EditButton As DataGridViewButtonColumn
@@ -122,4 +183,10 @@ Partial Class PaymentConditionList
     Friend WithEvents CutOff As DataGridViewTextBoxColumn
     Friend WithEvents PaymentMonth As DataGridViewTextBoxColumn
     Friend WithEvents PaymentDate As DataGridViewTextBoxColumn
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel As ToolStripStatusLabel
+    Friend WithEvents ToolTip As ToolTip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ClearSearcConditionButton As Button
+    Friend WithEvents SearchButton As Button
 End Class
--- Application/Form/PaymentConditionList.vb
+++ Application/Form/PaymentConditionList.vb
@@ -1,3 +1,9 @@
-﻿Public Class PaymentConditionList
+﻿Option Strict On
+Option Infer On
+
+''' <summary>
+''' 支払条件の管理を行う画面
+''' </summary>
+Public Class PaymentConditionList
 
 End Class

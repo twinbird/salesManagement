d85fd9a03f4a168a5b22828f9ebc449218d59c69
twinbird <ixa2063@gmail.com>
2018-01-12 05:21:06 +0900

    自社情報の登録用画面のUIとDomainオブジェクトを追加

--- Application/Form/CompanyInformationEntry.vb
+++ Application/Form/CompanyInformationEntry.vb
@@ -1,3 +1,177 @@
-﻿Public Class CompanyInformationEntry
+﻿Option Strict On
+Option Infer On
+
+''' <summary>
+''' 自社情報の設定画面
+''' </summary>
+Public Class CompanyInformationEntry
+
+#Region "メッセージ定数"
+
+    Private Const SaveErrorMessage As String = "保存に失敗しました。時間をおいてもう一度お試しください。"
+
+#End Region
+
+#Region "イベント"
+
+    ''' <summary>
+    ''' 画面ロードイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CustomerEntry_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetupControls()
+    End Sub
+
+    ''' <summary>
+    ''' 登録ボタンクリック
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub EntryButton_Click(sender As Object, e As EventArgs) Handles EntryButton.Click
+        '登録に成功したら画面を閉じる
+        If Save() = True Then
+            Me.Close()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' フォームをアクティベイト
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CustomerEntry_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        'コントロールのデータソースを更新
+        UpdateControlDataSource()
+    End Sub
+
+#End Region
+
+#Region "コントロール制御"
+
+    ''' <summary>
+    ''' フォーム内のコントロールの初期設定を実行
+    ''' </summary>
+    Private Sub SetupControls()
+        '=============================================================================
+        '各コントロールの表示設定
+        '=============================================================================
+        '営業担当者
+        SetupPICComboBox()
+        '支払条件
+        SetupPaymentConditionComboBox()
+
+        '=============================================================================
+        'ドメインオブジェクトをインスタンス化してフォームコントロールにバインディング
+        '=============================================================================
+
+        '編集用にオブジェクトが引き渡されている呼び出しでは引き渡されたインスタンスを利用する
+        If _Customer Is Nothing Then
+            '永続化用リポジトリのインスタンスを用意
+            Dim custRepo = New Infrastructure.CustomerRepositoryImpl
+            Dim payRepo = New Infrastructure.PaymentConditionRepositoryImpl
+            Dim empRepo = New Infrastructure.EmployeeRepositoryImpl
+            'ドメインオブジェクトを生成
+            _Customer = New Domain.Customer(custRepo, payRepo, empRepo)
+        End If
+        'バインディング
+        Me.BindingSource.DataSource = _Customer
+
+        '=============================================================================
+        '各コントロールとバインディングオブジェクトを紐づけ
+        '=============================================================================
+        '会社名
+        CustomerNameTextBox.DataBindings.Add(NameOf(CustomerNameTextBox.Text), BindingSource, NameOf(_Customer.Name))
+        'かな名
+        CustomerKanaNameTextBox.DataBindings.Add(NameOf(CustomerKanaNameTextBox.Text), BindingSource, NameOf(_Customer.KanaName))
+        '営業担当者
+        PICComboBox.DataBindings.Add(NameOf(PICComboBox.SelectedValue), BindingSource, NameOf(_Customer.PIC))
+        '支払条件
+        PaymentConditionComboBox.DataBindings.Add(NameOf(PaymentConditionComboBox.SelectedValue), BindingSource, NameOf(_Customer.PaymentCondition))
+        '郵便番号
+        PostalCodeMaskedTextBox.DataBindings.Add(NameOf(PostalCodeMaskedTextBox.Text), BindingSource, NameOf(_Customer.PostalCode))
+        '住所1
+        Address1TextBox.DataBindings.Add(NameOf(Address1TextBox.Text), BindingSource, NameOf(_Customer.Address1))
+        '住所2
+        Address2TextBox.DataBindings.Add(NameOf(Address2TextBox.Text), BindingSource, NameOf(_Customer.Address2))
+
+        '=============================================================================
+        'エラープロバイダのデータソースにフォームを紐づけたバインディングソースを割り当て
+        '=============================================================================
+        Me.ErrorProvider.DataSource = BindingSource
+    End Sub
+
+    ''' <summary>
+    ''' 営業担当者のコンボボックスを設定
+    ''' </summary>
+    Private Sub SetupPICComboBox()
+        Dim displayValues As New List(Of KeyValuePair(Of String, Domain.Employee))
+
+        '従業員情報を取得してコンボボックスメンバに利用
+        Dim repo = New Infrastructure.EmployeeRepositoryImpl
+        Dim emps = repo.FindAllEmployee
+        For Each e In emps
+            Dim kvp = New KeyValuePair(Of String, Domain.Employee)(e.Name, e)
+            displayValues.Add(kvp)
+        Next
+
+        'ComboBoxの設定
+        PICComboBox.ValueMember = "Value"
+        PICComboBox.DisplayMember = "Key"
+        PICComboBox.DataSource = displayValues
+    End Sub
+
+    ''' <summary>
+    ''' 支払条件のコンボボックスを設定
+    ''' </summary>
+    Private Sub SetupPaymentConditionComboBox()
+        Dim displayValues As New List(Of KeyValuePair(Of String, Domain.PaymentCondition))
+
+        '支払条件情報を取得してコンボボックスメンバに利用
+        Dim repo = New Infrastructure.PaymentConditionRepositoryImpl
+        Dim pays = repo.FindAllPaymentCondition
+        For Each e In pays
+            Dim kvp = New KeyValuePair(Of String, Domain.PaymentCondition)(e.Name, e)
+            displayValues.Add(kvp)
+        Next
+
+        'ComboBoxの設定
+        PaymentConditionComboBox.ValueMember = "Value"
+        PaymentConditionComboBox.DisplayMember = "Key"
+        PaymentConditionComboBox.DataSource = displayValues
+    End Sub
+
+    ''' <summary>
+    ''' コントロールのデータソースを更新
+    ''' </summary>
+    Private Sub UpdateControlDataSource()
+        SetupPICComboBox()
+        SetupPaymentConditionComboBox()
+    End Sub
+
+#End Region
+
+#Region "永続化"
+
+    ''' <summary>
+    ''' フォームの入力内容で登録処理を実行
+    ''' </summary>
+    ''' <returns>登録成功:True 登録失敗:False</returns>
+    Private Function Save() As Boolean
+        'ドメインオブジェクトの正当性を確認
+        If _Customer.Validate() = False Then
+            'エラー情報を更新
+            Me.ErrorProvider.UpdateBinding()
+            Return False
+        End If
+        '登録
+        If _Customer.Save() = False Then
+            MessageBox.Show(SaveErrorMessage)
+        End If
+        Return True
+    End Function
+
+#End Region
+
 
 End Class
--- Domain/DomainObject/CompanyInformation.vb
+++ Domain/DomainObject/CompanyInformation.vb
@@ -12,16 +12,143 @@ Public Class CompanyInformation
 
 #Region "定数"
 
+    Private Const NameDoNotEmpty As String = "会社名は必ず入力してください"
+    Private Const NameIsTooLong As String = "会社名は100文字以内で入力してください"
+    Private Const PostalCodeDoNotNothing As String = "郵便番号を未定義にすることはできません"
+    Private Const PostalCodeIsWrongFormat As String = "郵便番号の入力形式が間違っています"
+    Private Const Address1DoNotNothing As String = "住所1を未定義にすることはできません"
+    Private Const Address1IsTooLong As String = "住所1は50文字以内で入力してください"
+    Private Const Address2DoNotNothing As String = "住所2を未定義にすることはできません"
+    Private Const Address2IsTooLong As String = "住所2は50文字以内で入力してください"
+    Private Const TELDoNotNothing As String = "TELを未定義にすることはできません"
+    Private Const TELIsTooLong As String = "TELは15文字以内で入力してください"
+    Private Const TELIsWrongFormat As String = "TELの入力形式が間違っています"
+    Private Const FAXDoNotNothing As String = "FAXを未定義にすることはできません"
+    Private Const FAXIsTooLong As String = "FAXは15文字以内で入力してください"
+    Private Const FAXIsWrongFormat As String = "FAXの入力形式が間違っています"
+
 #End Region
 
 #Region "コンストラクタ"
 
     Public Sub New()
+        _ID = -1
+        _Name = String.Empty
+        _PostalCode = String.Empty
+        _Address1 = String.Empty
+        _Address2 = String.Empty
+        _TEL = String.Empty
+        _FAX = String.Empty
+    End Sub
+
+    Public Sub New(ByVal id As Integer)
+        _ID = id
+        _Name = String.Empty
+        _PostalCode = String.Empty
+        _Address1 = String.Empty
+        _Address2 = String.Empty
+        _TEL = String.Empty
+        _FAX = String.Empty
     End Sub
 
 #End Region
 
 #Region "値プロパティ"
+
+    ''' <summary>
+    ''' ID(オブジェクトの永続化がされていないものは-1)
+    ''' </summary>
+    Public ReadOnly Property ID As Integer
+
+    Private _Name As String
+    ''' <summary>
+    ''' 会社名
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Name As String
+        Get
+            Return _Name
+        End Get
+        Set(value As String)
+            _Name = value
+            ValidateName()
+        End Set
+    End Property
+
+    Private _PostalCode As String
+    ''' <summary>
+    ''' 郵便番号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property PostalCode As String
+        Get
+            Return _PostalCode
+        End Get
+        Set(value As String)
+            _PostalCode = value
+            ValidatePostalCode()
+        End Set
+    End Property
+
+    Private _Address1 As String
+    ''' <summary>
+    ''' 住所1
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Address1 As String
+        Get
+            Return _Address1
+        End Get
+        Set(value As String)
+            _Address1 = value
+            ValidateAddress1()
+        End Set
+    End Property
+
+    Private _Address2 As String
+    ''' <summary>
+    ''' 住所2
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Address2 As String
+        Get
+            Return _Address2
+        End Get
+        Set(value As String)
+            _Address2 = value
+            ValidateAddress2()
+        End Set
+    End Property
+
+    Private _TEL As String
+    ''' <summary>
+    ''' 電話番号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property TEL As String
+        Get
+            Return _TEL
+        End Get
+        Set(value As String)
+            _TEL = value
+            ValidateTEL()
+        End Set
+    End Property
+
+    Private _FAX As String
+    ''' <summary>
+    ''' FAX番号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property FAX As String
+        Get
+            Return _FAX
+        End Get
+        Set(value As String)
+            _FAX = value
+            ValidateFAX()
+        End Set
+    End Property
 
 #End Region
 
@@ -68,6 +195,155 @@ Public Class CompanyInformation
 #End Region
 
 #Region "検証メソッド"
+
+    ''' <summary>
+    ''' オブジェクトの整合性チェックを実施
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Validate() As Boolean
+        ValidateName()
+        ValidatePostalCode()
+        ValidateAddress1()
+        ValidateAddress2()
+        ValidateTEL()
+        ValidateFAX()
+
+        Return Me.HasError = False
+    End Function
+
+    ''' <summary>
+    ''' 会社名の検証
+    ''' </summary>
+    Private Sub ValidateName()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(Name))
+
+        '名前は未入力不可
+        If _Name Is Nothing OrElse _Name.Length = 0 Then
+            _errors(NameOf(Name)) = NameDoNotEmpty
+            Return
+        End If
+        '名前は100文字以内
+        If _Name.Length > 100 Then
+            _errors(NameOf(Name)) = NameIsTooLong
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 郵便番号の検証
+    ''' </summary>
+    Private Sub ValidatePostalCode()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(PostalCode))
+
+        'Nothingは許可しない
+        If _PostalCode Is Nothing Then
+            _errors(NameOf(PostalCode)) = PostalCodeDoNotNothing
+            Return
+        End If
+
+        '郵便番号の形式を確認
+        If Text.RegularExpressions.Regex.IsMatch(
+            _PostalCode,
+            "\A\d\d\d-\d\d\d\d\z",
+            Text.RegularExpressions.RegexOptions.ECMAScript) = False Then
+            _errors(NameOf(PostalCode)) = PostalCodeIsWrongFormat
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 住所1の検証
+    ''' </summary>
+    Private Sub ValidateAddress1()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(Address1))
+
+        'Nothingは許可しない
+        If _Address1 Is Nothing Then
+            _errors(NameOf(Address1)) = Address1DoNotNothing
+            Return
+        End If
+
+        '住所1は50文字まで
+        If _Address1.Length > 50 Then
+            _errors(NameOf(Address1)) = Address1IsTooLong
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 住所2の検証
+    ''' </summary>
+    Private Sub ValidateAddress2()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(Address2))
+
+        'Nothingは許可しない
+        If _Address1 Is Nothing Then
+            _errors(NameOf(Address2)) = Address2DoNotNothing
+            Return
+        End If
+
+        '住所1は50文字まで
+        If _Address2.Length > 50 Then
+            _errors(NameOf(Address2)) = Address2IsTooLong
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 電話番号の検証
+    ''' </summary>
+    Private Sub ValidateTEL()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(TEL))
+
+        'Nothingは許可しない
+        If _TEL Is Nothing Then
+            _errors(NameOf(TEL)) = TELDoNotNothing
+            Return
+        End If
+
+        'TELは15文字まで
+        If _TEL.Length > 15 Then
+            _errors(NameOf(TEL)) = TELIsTooLong
+        End If
+
+        '電話番号っぽいかチェック
+        If _TEL <> String.Empty AndAlso Text.RegularExpressions.Regex.IsMatch(
+            _TEL,
+            "\A0\d{1,4}-\d{1,4}-\d{4}\z") Then
+            _errors(NameOf(TEL)) = TELIsWrongFormat
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' FAX番号の検証
+    ''' </summary>
+    Private Sub ValidateFAX()
+        'エラーを一度クリア
+        _errors.Remove(NameOf(FAX))
+
+        'Nothingは許可しない
+        If _FAX Is Nothing Then
+            _errors(NameOf(FAX)) = FAXDoNotNothing
+            Return
+        End If
+
+        'FAXは15文字まで
+        If _FAX.Length > 15 Then
+            _errors(NameOf(FAX)) = FAXIsTooLong
+        End If
+
+        'FAX番号っぽいかチェック
+        If _FAX <> String.Empty AndAlso Text.RegularExpressions.Regex.IsMatch(
+            _FAX,
+            "\A0\d{1,4}-\d{1,4}-\d{4}\z") Then
+            _errors(NameOf(FAX)) = FAXIsWrongFormat
+        End If
+
+    End Sub
 
 #End Region
 

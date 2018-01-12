7a1cdffb6376ad7b57305c5e47d0d3996dc9c461
twinbird <ixa2063@gmail.com>
2018-01-12 11:06:58 +0900

    デザイナを修正

--- Application/Form/SalesTaxRateEntry.Designer.vb
+++ Application/Form/SalesTaxRateEntry.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class SalesTaxRateEntry
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,21 +20,23 @@ Partial Class SalesTaxRateEntry
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ApplyDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Rate = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.EntryButton = New System.Windows.Forms.Button()
-        Me.FormTitleLabel = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ApplyDate, Me.Rate})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 46)
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 9)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 21
         Me.DataGridView1.Size = New System.Drawing.Size(265, 298)
@@ -52,36 +54,25 @@ Partial Class SalesTaxRateEntry
         '
         'EntryButton
         '
-        Me.EntryButton.Location = New System.Drawing.Point(190, 350)
+        Me.EntryButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.EntryButton.Location = New System.Drawing.Point(190, 313)
         Me.EntryButton.Name = "EntryButton"
         Me.EntryButton.Size = New System.Drawing.Size(87, 26)
         Me.EntryButton.TabIndex = 1
         Me.EntryButton.Text = "登録"
         Me.EntryButton.UseVisualStyleBackColor = True
         '
-        'FormTitleLabel
-        '
-        Me.FormTitleLabel.AutoSize = True
-        Me.FormTitleLabel.Font = New System.Drawing.Font("MS UI Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.FormTitleLabel.Location = New System.Drawing.Point(12, 9)
-        Me.FormTitleLabel.Name = "FormTitleLabel"
-        Me.FormTitleLabel.Size = New System.Drawing.Size(156, 24)
-        Me.FormTitleLabel.TabIndex = 2
-        Me.FormTitleLabel.Text = "消費税の設定"
-        '
         'SalesTaxRateEntry
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(289, 388)
-        Me.Controls.Add(Me.FormTitleLabel)
+        Me.ClientSize = New System.Drawing.Size(289, 350)
         Me.Controls.Add(Me.EntryButton)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "SalesTaxRateEntry"
         Me.Text = "消費税の設定"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -89,5 +80,4 @@ Partial Class SalesTaxRateEntry
     Friend WithEvents ApplyDate As DataGridViewTextBoxColumn
     Friend WithEvents Rate As DataGridViewTextBoxColumn
     Friend WithEvents EntryButton As Button
-    Friend WithEvents FormTitleLabel As Label
 End Class

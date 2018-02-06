b9a3a7c2ade54847843466145ed9b371ac195e36
twinbird <ixa2063@gmail.com>
2018-02-06 16:37:02 +0900

    MSReportにかかわる参照を追加

---
+++ Application/Form/EstimateReportViewer.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EstimateReportViewer
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ReportViewer = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.SuspendLayout()
+        '
+        'ReportViewer
+        '
+        Me.ReportViewer.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer.Name = "ReportViewer"
+        Me.ReportViewer.ServerReport.BearerToken = Nothing
+        Me.ReportViewer.Size = New System.Drawing.Size(396, 246)
+        Me.ReportViewer.TabIndex = 0
+        '
+        'EstimateReportViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 528)
+        Me.Name = "EstimateReportViewer"
+        Me.Text = "見積書プレビュー"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ReportViewer As Microsoft.Reporting.WinForms.ReportViewer
+End Class
---
+++ Application/Form/EstimateReportViewer.vb
@@ -0,0 +1,3 @@
+﻿Public Class EstimateReportViewer
+
+End Class

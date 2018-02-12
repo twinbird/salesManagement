2d3342141ba5c3378198316f636fa44d018bdf58
twinbird <ixa2063@gmail.com>
2018-02-13 05:16:08 +0900

    テストを追加

--- TestDomain/TestCompanyInformation.vb
+++ TestDomain/TestCompanyInformation.vb
@@ -22,4 +22,28 @@ Imports Domain
         Assert.IsTrue(c.HasError)
     End Sub
 
+    <TestMethod()> Public Sub TestNameMaxLength()
+        Dim c = New CompanyInformation
+
+        Dim maximum = ""
+        For i = 1 To 100
+            maximum &= "あ"
+        Next
+
+        With c
+            .Name = maximum
+            .PostalCode = "100-8111"
+            .Address1 = "東京都千代田区"
+            .Address2 = "千代田1-1"
+            .TEL = "03-9999-9999"
+            .FAX = "03-8888-8888"
+        End With
+        Assert.IsTrue(c.Validate)
+        Assert.IsFalse(c.HasError)
+
+        c.Name &= "あ"
+        Assert.IsFalse(c.Validate)
+        Assert.IsTrue(c.HasError)
+    End Sub
+
 End Class

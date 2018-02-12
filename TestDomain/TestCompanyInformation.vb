fb8cb671575a2624f31f90dfeb34d18fa57bc84b
twinbird <ixa2063@gmail.com>
2018-02-13 04:27:54 +0900

    自社情報オブジェクトにテストを追加

--- TestDomain/TestCompanyInformation.vb
+++ TestDomain/TestCompanyInformation.vb
@@ -1,9 +1,25 @@
-﻿Imports System.Text
+﻿Option Strict On
+Option Infer On
+
+Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Domain
 
 <TestClass()> Public Class TestCompanyInformation
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestNameShouldNotEmpty()
+        Dim c = New CompanyInformation
+        With c
+            .Name = ""
+            .PostalCode = "100-8111"
+            .Address1 = "東京都千代田区"
+            .Address2 = "千代田1-1"
+            .TEL = "03-9999-9999"
+            .FAX = "03-8888-8888"
+        End With
+
+        Assert.IsFalse(c.Validate)
+        Assert.IsTrue(c.HasError)
     End Sub
 
 End Class

9287af71e3bcc95e8402609c2c3267a62fab1896
twinbird <ixa2063@gmail.com>
2018-02-15 23:14:36 +0900

    テストを追加

--- TestInfrastructure/TestCompanyInformation.vb
+++ TestInfrastructure/TestCompanyInformation.vb
@@ -1,9 +1,33 @@
-﻿Imports System.Text
+﻿Option Strict On
+Option Infer On
+
+Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Domain
+Imports Infrastructure
 
 <TestClass()> Public Class TestCompanyInformation
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestInitialize()> Public Sub Setup()
+        Dim setting = New InfrastructureSetting
+        setting.InitializeDB()
+    End Sub
+
+    <TestMethod()> Public Sub TestSaveAndLoad()
+        Dim repo = New CompanyInformationImpl
+        Dim c = New CompanyInformation
+        With c
+            .Name = "company name"
+            .PostalCode = "001-0001"
+            .Address1 = "Address1"
+            .Address2 = "Address2"
+            .TEL = "00-0000-0000"
+            .FAX = "00-0000-0000"
+        End With
+
+        Assert.IsTrue(repo.Save(c))
+        Assert.AreEqual(repo.Find, repo.Find)
+
     End Sub
 
 End Class
